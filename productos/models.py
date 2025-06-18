a5bffcfabc7aa623e63b2b55b7320afcae9381dd
Santiago <prueba@gmail.com>
2025-06-18 10:33:22 -0500

    Se agregó el modelo Producto y pruebas unitarias

--- productos/models.py
+++ productos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
 
     def __str__(self):
